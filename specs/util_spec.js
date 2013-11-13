af2950088303531a1c4a8f8e7205224792964ca7
Evan Briones <erbriones@gmail.com>
2013-11-12 17:06:20 -0700

    add rotate and transform helpers

--- specs/util_spec.js
+++ specs/util_spec.js
@@ -3,4 +3,14 @@ describe("Bioplot: util", function() {
         var result = translate(25, 25);
         expect(result).toBe("translate(25,25)");
     });
+
+    it("should be a svg rotate string", function() {
+        var result = rotate(25);
+        expect(result).toBe("rotate(25)");
+    });
+
+    it("shold be a rotate and transform string", function() {
+        var result = translate(25, 25) + rotate(25);
+        expect(transform(25,25,25)).toBe(result);
+    });
 })
--- src/util.js
+++ src/util.js
@@ -1,3 +1,11 @@
 function translate(x, y) {
     return "translate(" + x + "," + y + ")";
 }
+
+function rotate(rotation) {
+    return "rotate(" + rotation + ")";
+}
+
+function transform(x, y, rotation) {
+    return translate(x,y) + rotate(rotation);
+}
