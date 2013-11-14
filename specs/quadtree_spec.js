df2aec988826be6140b4798752c5eb520fc5a46c
Evan Briones <erbriones@gmail.com>
2013-11-14 12:51:03 -0700

    added rectangle

---
+++ specs/quadtree_spec.js
@@ -0,0 +1,37 @@
+describe("quadtree", function() {
+    describe("rectangle", function() {
+        var w = new rectangle(5, 5, 5, 5),
+            x = new rectangle(0, 0, 20, 20),
+            y = new rectangle(10, 15, 20, 20),
+            z = new rectangle(25, 25, 10, 10);
+
+        it("should intersect itself", function() {
+            expect(x.intersects(x)).toBe(true);
+        })
+
+        it("should intersect rectangle", function() {
+            expect(x.intersects(y)).toBe(true);
+            expect(y.intersects(x)).toBe(true);
+            expect(w.intersects(x)).toBe(true);
+            expect(x.intersects(w)).toBe(true);
+        })
+
+        it("should not intersect rectangle", function() {
+            expect(w.intersects(w)).toBe(true);
+            expect(x.intersects(z)).toBe(false);
+            expect(z.intersects(x)).toBe(false);
+        })
+
+        it("should contain itself", function() {
+            expect(x.contains(x)).toBe(true);
+        })
+
+        it("should contain the rectangle", function() {
+            expect(x.contains(w)).toBe(true);
+        })
+
+        it("should not contain the rectangle", function() {
+            expect(w.contains(x)).toBe(false);
+        })
+    })
+})
---
+++ src/quadtree.js
@@ -0,0 +1,24 @@
+function rectangle(x, y, width, height) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+
+    this.contains = function(that) {
+        return (that.x >= this.x &&
+                that.y >= this.y &&
+                (that.x + that.width) <= (this.x + this.width) &&
+                (that.y + that.height) <= (this.y + this.height));
+    };
+
+    this.toString = function() {
+        return "[x=" + this.x  + ", y=" + this.y + ", width=" + this.width + ", height=" + this.height + "]";
+    };
+
+    this.intersects = function(that) {
+        return !(that.left > (this.left + this.width) ||
+                 (that.left + that.width) < this.left ||
+                 that.y > (this.y + this.height) ||
+                 (that.y + that.height) < this.x);
+    };
+}
