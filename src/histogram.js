3a267605fdfcddcdbfe0deca0deb4a7e3175f595
Evan Briones <erbriones@gmail.com>
2013-11-19 16:56:00 -0700

    added bin redrawing

--- src/histogram.js
+++ src/histogram.js
@@ -5,6 +5,7 @@ function histogram(element, config) {
         horizontal: d3.scale.linear(),
         vertical: d3.scale.linear()
     };
+    this.bins = [];
 
     this.configure = function (configuration) {
         this.config = configuration || {};
@@ -13,14 +14,35 @@ function histogram(element, config) {
             height: 700
         };
         this.config.bins = this.config.bins || 50;
+
+        return self;
     };
 
     this.render = function() {
-        this.plot = d3.select(element).append("svg")
+        self.plot = d3.select(element).append("svg")
                     .attr("width", self.config.size.width)
                     .attr("height", self.config.size.height);
 
         return self;
+    };
+
+    this.redraw = function() {
+        if (!self.bins.length) return;
+
+        var xScale = self.scales.horizontal,
+            yScale = self.scales.vertical;
+
+        var bars = self.plot.selectAll("rect").data(self.bins);
+
+        bars.enter().append("rect");
+
+        bars.transition()
+            .attr("x", function(d) { return xScale(d.x); })
+            .attr("y", function(d) { return yScale(d.y); })
+            .attr("width", function(d) { return xScale(d.dx); })
+            .attr("height", function(d) { return yScale.range()[0] - yScale(d.y); });
+
+        bars.exit().remove();
     };
 
     this.load = function(data) {
@@ -32,6 +54,8 @@ function histogram(element, config) {
 
         self.scales.vertical.domain([0, d3.max(self.bins, function(bin) { return bin.y; })])
             .range([self.config.size.height - 20, 20]);
+
+        return self;
     };
 
     this.configure(config);
