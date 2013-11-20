d44e1c85ef23e16fe9b184cc6f74b77e97b4090e
Evan Briones <erbriones@gmail.com>
2013-11-20 15:01:51 -0700

    added x-axis

--- src/histogram.js
+++ src/histogram.js
@@ -23,13 +23,19 @@ function histogram(element, config) {
 
     this.render = function() {
         self.svg = d3.select(element).append("svg")
-                    .attr("width", self.config.size.width)
-                    .attr("height", self.config.size.height);
+                    .attr("width", self.config.size.width + 20)
+                    .attr("height", self.config.size.height + 20)
+                    .append("g")
+                    .attr("transform", translate(20, 20));
 
         self.plot = self.svg.append("g").attr("class", "bins");
         self.brush = d3.svg.brush();
         self.selected = self.svg.append("g")
             .attr("class", "brush");
+
+        self.xaxis = self.svg.append("g")
+            .attr("class", "axis")
+            .attr("transform", translate(0, self.config.size.height - 20));
 
         return self;
     };
@@ -53,13 +59,16 @@ function histogram(element, config) {
 
         bars.exit().remove();
 
+        var axis = d3.svg.axis().scale(xScale).orient("bottom");
+        self.xaxis.call(axis);
+
         self.brush.clear()
             .x(xScale)
             .on("brush", self._onSelectionChange(self.brush));
 
         self.selected
             .call(self.brush)
-            .selectAll("rect").attr("height", self.config.size.height);
+            .selectAll("rect").attr("height", self.config.size.height - 20);
     };
 
     this.bin = function(data) {
