7b6d39de139de75b93903c49ea73e5686506ef40
Evan Briones <erbriones@gmail.com>
2013-12-10 16:48:35 -0700

    added scan and pick

--- specs/util_spec.js
+++ specs/util_spec.js
@@ -1,4 +1,4 @@
-describe("Bioplot: util", function() {
+describe("util", function() {
     it("should be a svg translate string", function() {
         var result = translate(25, 25);
         expect(result).toBe("translate(25,25)");
@@ -13,4 +13,24 @@ describe("Bioplot: util", function() {
         var result = translate(25, 25) + rotate(25);
         expect(transform(25,25,25)).toBe(result);
     });
+
+    it("should pick out the attribute", function() {
+        var x = {
+            a: 'a',
+            b: 'b',
+            c: 'c'
+        };
+
+        expect(pick('a')(x)).toBe('a');
+        expect(pick('b')(x)).toBe('b');
+        expect(pick('c')(x)).toBe('c');
+    });
+
+    it("should scan the values", function() {
+        var points = [10, 5, 2, 2, 5];
+        var add = function(a, b) { return a + b; }
+        var divide = function(a, b) { return a / b; }
+        expect(scan(add, 0, points)).toEqual([0, 10, 15, 17, 19, 24])
+        expect(scan(divide, 1000, points)).toEqual([1000, 100, 20, 10, 5, 1]);
+    })
 })
--- src/util.js
+++ src/util.js
@@ -1,3 +1,5 @@
+var slice = Array.prototype.slice;
+
 function translate(x, y) {
     return "translate(" + x + "," + y + ")";
 }
@@ -8,4 +10,18 @@ function rotate(rotation) {
 
 function transform(x, y, rotation) {
     return translate(x,y) + rotate(rotation);
+}
+
+function pick(attribute) {
+    return function(object) {
+        return object[attribute];
+    };
+}
+
+function scan(func, initial, iterable) {
+    var reduce = function(value, index, list) {
+        return list.slice(0, index + 1).reduce(func);
+    };
+
+    return [].concat([initial], iterable).map(reduce);
 }
